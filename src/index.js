7ec3327e52124dfdaa2ed17b4db01b1eefe6414b
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-12-10 21:53:54 +0200

    hw-11-1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/main.scss';
+import Notiflix from 'notiflix';
+
+const searchForm = document.querySelector(".search-form");
+const input = document.querySelector(".text-input");
+const button = document.querySelector(".submit");
+
+
+
